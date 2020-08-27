f7a0ab89fb69d11bdb9e0953dfdd53a7a4e10fa4
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-08-27 09:29:19 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,21 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.new(name: "First Ingredient")
+    @recipe.ingredients.new(name: "Second Ingredient")
   end
 
   def create
+    Recipe.create(recipe_params)
   end
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [
+        :name,
+        :quantity
+    ] )
+  end
+
+
 end
